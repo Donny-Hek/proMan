58ccef86db2178601570b9dcdf7826edef743ca5
user <klewernoe@gmail.com>
2023-09-17 13:49:13 +0300

    maven sources

---
+++ src/main/java/com/pm/proman/ProManApplication.java
@@ -0,0 +1,12 @@
+package com.pm.proman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProManApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pm/proman/ProManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.proman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
