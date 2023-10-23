dfde94694f626d3b394d56e4f4b83eb62d485316
user <klewernoe@gmail.com>
2023-10-24 00:23:40 +0300

    success sidebar

--- src/main/webapp/src/layout/app.layout.jsx
+++ src/main/webapp/src/layout/app.layout.jsx
@@ -4,8 +4,25 @@ import authService from "../service/auth.service";
 import Sidebar from "./sidebar";
 
 export default function AppLayout() {
+  // const navigate = useNavigate();
+  // const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const checkAuth = async () => {
+  //     const user = await authService.getCurrentUser();
+  //     if (!user) {
+  //       navigate("/login");
+  //     } else {
+  //       // save user
+  //       dispatch(setUser(user));
+  //       setLoading(false);
+  //     }
+  //   };
+  //   checkAuth();
+  // }, [navigate]);
+
   return (
-    <div>
+    <div >
       <Sidebar />
     </div>
   );
--- src/main/webapp/src/layout/sidebar.js
+++ src/main/webapp/src/layout/sidebar.js
@@ -5,7 +5,6 @@ import { Outlet } from "react-router-dom";
 import { withRouter } from "../common/with-router";
 import projectsService from "../service/projects.service";
 import userService from "../service/user.service";
-import { useState } from "react";
 
 class Sidebar extends Component {// extends Component 
   constructor(props) {
@@ -54,66 +53,54 @@ class Sidebar extends Component {// extends Component
 
   render() {
     return (
-      <div>
-        <div class="container-fluid">
-          <div class="row flex-nowrap">
-            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0  bg-dark">
-              <div class="d-flex flex-column align-items-center align-items-sm-start bg-dark px-3 pt-2 link-light min-vh-100">
-                {/* <span class="fs-5 d-none d-sm-inline">ProMan</span> */}
-                <div class="nav nav-pills flex-column scrollarea mb-sm-auto mb-0 align-items-center align-items-sm-start">
-                  <button
-                    class="btn btn-outline-light"
-                    onClick={this.addBoard}
-                  >
-                    Новый проект
-                  </button>
-                  {this.state.message && (
-                    <li class="list-group-item">
-                      <span class="px-0 align-middle">{this.state.message}</span>
-                    </li>
-                  )}
-                  {
-                    Array.isArray(this.state.links) ?
-                      this.state.links.map(board => {
-                        return (<li class="list-group-item">
-                          <a href={"/board/" + board.id} class="nav-link px-0 align-middle ">{board.name}</a>
-                        </li>)
-                      })
-                      : null
-                  }
-                </div>
-
-                <div class="dropdown pb-2">
-                  <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
-                    <li>
-                      <a
-                        href="/profile"
-                        class="nav-link px-0 align-middle link-light"
-                      >
-                        Профиль
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="/login"
-                        class="nav-link px-0 align-middle link-light"
-                        onClick={() => {
-                          authService.logout();
-                        }}
-                      >
-                        Выйти
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="col py-3">
-              <Outlet />
-            </div>
+      <div class="d-flex flex-nowrap">
+        <div class="d-flex flex-column px-sm-2 px-3 pt-2
+        bg-dark vh-100 overflow-hidden" style={{ width: '230px', }}>
+          {/*кнопки*/}
+          <div class="flex-column align-items-center nav">
+            <a
+              href="/profile"
+              class="nav-link px-0 align-middle link-light"
+            >
+              Профиль
+            </a>
+            <a
+              href="/login"
+              class="nav-link px-0 align-middle link-warning"
+              onClick={() => {
+                authService.logout();
+              }}
+            >
+              Выйти
+            </a>
+            <button
+              class="btn btn-outline-light"
+              onClick={this.addBoard}
+            >
+              Новый проект
+            </button>
+          </div>
+          {/*список*/}
+          <div class="overflow-y-auto flex-column flex-nowrap nav px-3 pt-2">
+            {this.state.message && (
+              <li class="list-group-item">
+                <span class="px-0 align-middle link-light">{this.state.message}</span>
+              </li>
+            )}
+            {Array.isArray(this.state.links) ?
+              this.state.links.map(board => {
+                return (<li class="list-group-item">
+                  <a href={"/board/" + board.id} class="nav-link px-0 align-middle ">{board.name}</a>
+                </li>)
+              })
+              : null
+            }
           </div>
         </div>
-      </div >
+        <div class="px-3 pt-2">
+          <Outlet />
+        </div>
+      </div>
     );
   }
 }
